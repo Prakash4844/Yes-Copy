ddd69fd082fd3f3648986831ad4aeaea36a1a384
Prakash4844 <pk484442@gmail.com>
2023-05-31 19:00:25 +0530

    Opens Windows at mouse position.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import CTkMessagebox
-import customtkinter as Ctk
+import customtkinter as ctk
 import pyperclip as pyclip
 import yaml
 
@@ -9,3 +9,33 @@ with open("config.yaml", "r") as f:
 
 clip_history_length = config["APP"]["HISTORY_LENGTH"]
 win_shortcut = config["APP"]["WINDOW_SHORTCUT"]
+
+ctk.set_appearance_mode("System")  # Set appearance mode to system
+ctk.set_default_color_theme("blue")  # Set default color theme to blue
+
+# Create the root window
+app = ctk.CTk()  # create CTk window like you do with the Tk window
+# Hide the window initially
+app.withdraw()
+
+app.geometry("350x400")
+app.resizable(False, False)
+app.title("Yes Copy")
+# Remove the window decoration
+app.overrideredirect(True)
+
+# Get the current mouse position
+mouse_x = app.winfo_pointerx()
+mouse_y = app.winfo_pointery()
+
+# Calculate the window position relative to the mouse
+window_x = mouse_x - app.winfo_width() // 2
+window_y = mouse_y - app.winfo_height() // 2
+
+# Set the window position
+app.geometry(f"+{window_x}+{window_y}")
+
+# Show the window
+app.deiconify()
+
+app.mainloop()
