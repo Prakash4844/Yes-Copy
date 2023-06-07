ce0a8dce05cc943b5661ac4d26745cd9830c4af3
Prakash4844 <pk484442@gmail.com>
2023-06-07 17:12:44 +0530

    Cleaned Main and added Dummy populate clipboard

--- clip.py
+++ clip.py
@@ -1,10 +1,11 @@
 from customtkinter import *
-from tkinter import *
+import tkinter as tk
 
 
 class ClipBoard(CTk):
-    def __init__(self):
+    def __init__(self, data):
         super().__init__()
+        self.clip_data = data
         set_appearance_mode("System")  # Set appearance mode to system
         set_default_color_theme("blue")  # Set default color theme to blue
         self.title("Yes Copy")
@@ -18,6 +19,29 @@ class ClipBoard(CTk):
         self.mouse_y = 0
         self.window_x = 0
         self.window_y = 0
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+    def populate_clipboard(self):
+        rel_y = 0.1
+        for clip_text in self.clip_data['clip']:
+            text_box = CTkTextbox(self, width=310, height=50)
+            text_box.place(relx=0.5, rely=rel_y, anchor=tk.CENTER)
+            text_box.configure(state="normal")
+            text_box.delete("0.0", tk.END)
+            text_box.configure(font=("Liberation Mono", 15))
+            text_box.insert("0.0", clip_text)
+            text_box.configure(state="disabled")
+            text_box.update()
+            copy_button = CTkButton(self, text="Copy", fg_color="green",
+                                    hover_color="dark green",
+                                    command=self.set_clip_head,
+                                    height=10, width=10)
+            copy_button.place(relx=0.85, rely=rel_y, anchor=tk.CENTER)
+            rel_y += 0.14
+
+    def set_clip_head(self):
+        pass
 
     def show_window(self):
         # Get the current mouse position
@@ -30,10 +54,7 @@ class ClipBoard(CTk):
 
         self.geometry(f"+{self.window_x}+{self.window_y}")
         self.deiconify()
-        # populate_clipboard()
+        self.populate_clipboard()
 
     def on_closing(self):
         self.withdraw()
-
-    def set_clip_head(self):
-        pass
--- main.py
+++ main.py
@@ -16,28 +16,12 @@ win_shortcut = config["APP"]["WINDOW_SHORTCUT"]
 ctk.set_appearance_mode("System")  # Set appearance mode to system
 ctk.set_default_color_theme("blue")  # Set default color theme to blue
 
+with open('clip.json') as file:
+    data = json.load(file)
+
+
 # Create the root window
-app = clip.ClipBoard()
-
-
-def populate_clipboard():
-    with open('clip.json') as file:
-        data = json.load(file)
-        rel_y = 0.1
-        for clip in data['clip']:
-            text_box = ctk.CTkTextbox(app, width=320, height=50)
-            text_box.place(relx=0.5, rely=rel_y, anchor=tk.CENTER)
-            text_box.configure(state="normal")
-            text_box.delete("0.0", tk.END)
-            text_box.configure(font=("Liberation Mono", 15))
-            text_box.insert("0.0", clip)
-            text_box.configure(state="disabled")
-            text_box.update()
-            copy_button = ctk.CTkButton(app, text="Copy", fg_color="green", hover_color="dark green",
-                                        command=app.set_clip_head,
-                                        height=10, width=10)
-            copy_button.place(relx=0.85, rely=rel_y, anchor=tk.CENTER)
-            rel_y += 0.14
+app = clip.ClipBoard(data)
 
 
 with keyboard.GlobalHotKeys({'<cmd_l>+v': app.show_window}) as hotkeys:
